5b9ff079f7d3117629888b809a7699fc5cd44ba9
Rafael Rojas <rafaelrojas@Rafaels-MacBook-Air.local>
2023-09-26 10:48:36 -0600

    list users output changes

--- lambda/listUsers/output.tf
+++ lambda/listUsers/output.tf
@@ -1,7 +1,7 @@
-output "createuser_function_arn" {
+output "listusers_function_arn" {
   value = aws_lambda_function.ListUsersHandler.invoke_arn
 }
 
-output "createuser_function_name" {
+output "listusers_function_name" {
    value = aws_lambda_function.ListUsersHandler.function_name
 }
