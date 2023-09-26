69ccb9e7ab8bcfa8bc85d2113205f59bd08415bc
Rafael Rojas <rafaelrojas@Rafaels-MacBook-Air.local>
2023-09-26 09:38:35 -0600

    modified lambdas to accept paths

--- lambda/createUser/README.md
+++ lambda/createUser/README.md
@@ -4,19 +4,13 @@ Sample payload for lambda:
 
 ~~~
 {
-  "body": "{\"id\":\"7\",\"firstName\":\"John\",\"lastName\":\"Doe\"}"
+  "routeKey": "POST /users",
+  "body": "{\"id\":\"10\",\"firstName\":\"John\",\"lastName\":\"Doe\"}"
 }
-
 ~~~
 
 Sample payload restAPI
 
 ~~~
-curl -X POST "{api endpoint}/dev/users" -H 'Content-Type: application/json' -d'
-{
-  "id": "1",
-  "firstName": "somename",
-  "lastName": "someLastName"
-}
-‘
+TBD
 ~~~
--- lambda/createUser/create_user_lambda.py
+++ lambda/createUser/create_user_lambda.py
@@ -2,32 +2,43 @@ import logging
 import boto3
 import json
 import os
+
 session = boto3.Session(region_name=os.environ['REGION'])
 dynamodb_client = session.client('dynamodb')
+
 def lambda_handler(event, context):
     try:
         print("event ->" + str(event))
-        payload = json.loads(event["body"])
-        print("payload ->" + str(payload))
-        dynamodb_response = dynamodb_client.put_item(
-            TableName=os.environ["USERS_TABLE"],
-            Item={
-                "id": {
-                    "S": payload["id"]
-                },
-                "firstName": {
-                    "S": payload["firstName"]
-                },
-                "lastName": {
-                    "S": payload["lastName"]
+        
+        route_key = event.get('routeKey')
+        
+        if route_key == 'POST /users':
+            payload = json.loads(event["body"])
+            print("payload ->" + str(payload))
+            dynamodb_response = dynamodb_client.put_item(
+                TableName=os.environ["USERS_TABLE"],
+                Item={
+                    "id": {
+                        "S": payload["id"]
+                    },
+                    "firstName": {
+                        "S": payload["firstName"]
+                    },
+                    "lastName": {
+                        "S": payload["lastName"]
+                    }
                 }
+            )
+            print(dynamodb_response)
+            return {
+                'statusCode': 201,
+                'body': '{"status":"User created"}'
             }
-        )
-        print(dynamodb_response)
-        return {
-            'statusCode': 201,
-           'body': '{"status":"User created"}'
-        }
+        else:
+            return {
+                'statusCode': 400,
+                'body': '{"status":"Bad Request", "error":"Unsupported route"}'
+            }
     except Exception as e:
         logging.error(e)
         return {
