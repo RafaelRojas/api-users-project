437883c51f13451555b7364d2b06ec83d6e9631d
Rafael Rojas <rafaelrojas@Rafaels-MacBook-Air.local>
2023-09-26 11:16:26 -0600

    modify all users work

--- apiGateway/main.tf
+++ apiGateway/main.tf
@@ -77,12 +77,39 @@ resource "aws_apigatewayv2_route" "list_users_route" {
 resource "aws_lambda_permission" "apigw_list_users" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = data.terraform_remote_state.listUserslambda.outputs.listusers_function_name
+  function_name = data.terraform_remote_state.modifyUserlambda.outputs.modify_user_function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.users_api_gw.execution_arn}/*/GET/users"
 }
 ## list users block ends
+
+##modify user block starts
+resource "aws_apigatewayv2_integration" "modify_user_integration" {
+  api_id                 = aws_apigatewayv2_api.users_api_gw.id
+  integration_type       = "AWS_PROXY"
+  connection_type        = "INTERNET"
+  integration_method     = "POST"
+  integration_uri        = data.terraform_remote_state.modifyUserlambda.outputs.modify_user_function_arn
+  passthrough_behavior   = "WHEN_NO_MATCH"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "modify_users_route" {
+  api_id    = aws_apigatewayv2_api.users_api_gw.id
+  route_key = "PUT /users/{id}"
+  target    = "integrations/${aws_apigatewayv2_integration.modify_user_integration.id}"
+}
+
+resource "aws_lambda_permission" "apigw_modify_users" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = data.terraform_remote_state.modifyUserlambda.outputs.modify_user_function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.users_api_gw.execution_arn}/*/PUT/users/{id}"
+}
+##modify user block ends
 
 ####Stage with logs
 resource "aws_cloudwatch_log_group" "api_users_cwg" {
@@ -115,79 +142,6 @@ resource "aws_apigatewayv2_stage" "dev" {
   }
 }
 
-#y que jalara, tu
 output "api_gateway_url" {
   value = aws_apigatewayv2_api.users_api_gw.api_endpoint
 }
-
-
-
-
-
-
-# resource "aws_api_gateway_rest_api" "users_apigw" {
-#   name        = "users_apigw"
-#   description = "Product API Gateway"
-#   endpoint_configuration {
-#     types = ["REGIONAL"]
-#   }
-# }
-# resource "aws_api_gateway_resource" "users" {
-#   rest_api_id = aws_api_gateway_rest_api.users_apigw.id
-#   parent_id   = aws_api_gateway_rest_api.users_apigw.root_resource_id
-#   path_part   = "users"
-# }
-# resource "aws_api_gateway_method" "createuser" {
-#   rest_api_id   = aws_api_gateway_rest_api.users_apigw.id
-#   resource_id   = aws_api_gateway_resource.users.id
-#   http_method   = "POST"
-#   authorization = "NONE"
-# }
-
-# resource "aws_api_gateway_method" "deleteuser" {
-#   rest_api_id   = aws_api_gateway_rest_api.users_apigw.id
-#   resource_id   = aws_api_gateway_resource.users.id
-#   http_method   = "DELETE"
-#   authorization = "NONE"
-# }
-
-# resource "aws_api_gateway_integration" "createuser-lambda" {
-# rest_api_id = aws_api_gateway_rest_api.users_apigw.id
-#   resource_id = aws_api_gateway_method.createuser.resource_id
-#   http_method = aws_api_gateway_method.createuser.http_method
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri = data.terraform_remote_state.createUserlambda.outputs.createuser_function_arn
-# }
-
-# resource "aws_api_gateway_integration" "deleteuser-lambda" {
-# rest_api_id = aws_api_gateway_rest_api.users_apigw.id
-#   resource_id = aws_api_gateway_method.createuser.resource_id
-#   http_method = aws_api_gateway_method.deleteuser.http_method
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri = data.terraform_remote_state.deleteUserlambda.outputs.deleteuser_function_arn
-# }
-
-# resource "aws_lambda_permission" "apigw-CreateUserHandler" {
-#   action        = "lambda:InvokeFunction"
-#   function_name = data.terraform_remote_state.createUserlambda.outputs.createuser_function_name
-#   principal     = "apigateway.amazonaws.com"
-#   source_arn = "${aws_api_gateway_rest_api.users_apigw.execution_arn}/*/users"
-# }
-
-# resource "aws_lambda_permission" "apigw-DeleteUserHandler" {
-#   action        = "lambda:InvokeFunction"
-#   function_name = data.terraform_remote_state.deleteUserlambda.outputs.deleteuser_function_name
-#   principal     = "apigateway.amazonaws.com"
-#   source_arn = "${aws_api_gateway_rest_api.users_apigw.execution_arn}/*/users"
-# }
-
-
-# resource "aws_api_gateway_deployment" "usersapidev" {
-#   depends_on = [
-#     aws_api_gateway_integration.createuser-lambda
-#   ]
-#   rest_api_id = aws_api_gateway_rest_api.users_apigw.id
-#   stage_name  = "dev"
-# }
--- lambda/modifyUser/output.tf
+++ lambda/modifyUser/output.tf
@@ -1,7 +1,7 @@
-output "deleteuser_function_arn" {
+output "modify_user_function_arn" {
   value = aws_lambda_function.modifyUserHandler.invoke_arn
 }
 
-output "deleteuser_function_name" {
+output "modify_user_function_name" {
    value = aws_lambda_function.modifyUserHandler.function_name
 }
