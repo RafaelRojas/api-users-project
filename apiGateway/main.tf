8e292363522e3e074524ce6a68282364842c4bb6
Rafael Rojas <rafaelrojas@Rafaels-MacBook-Air.local>
2023-09-26 10:53:46 -0600

    get all users work

--- apiGateway/main.tf
+++ apiGateway/main.tf
@@ -57,7 +57,32 @@ resource "aws_lambda_permission" "apigw_delete" {
 }
 #delete user block ends
 
+##list users block start
+resource "aws_apigatewayv2_integration" "list_users_integration" {
+  api_id                 = aws_apigatewayv2_api.users_api_gw.id
+  integration_type       = "AWS_PROXY"
+  connection_type        = "INTERNET"
+  integration_method     = "POST"
+  integration_uri        = data.terraform_remote_state.listUserslambda.outputs.listusers_function_arn
+  passthrough_behavior   = "WHEN_NO_MATCH"
+  payload_format_version = "2.0"
+}
 
+resource "aws_apigatewayv2_route" "list_users_route" {
+  api_id    = aws_apigatewayv2_api.users_api_gw.id
+  route_key = "GET /users"
+  target    = "integrations/${aws_apigatewayv2_integration.list_users_integration.id}"
+}
+
+resource "aws_lambda_permission" "apigw_list_users" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = data.terraform_remote_state.listUserslambda.outputs.listusers_function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.users_api_gw.execution_arn}/*/GET/users"
+}
+## list users block ends
 
 ####Stage with logs
 resource "aws_cloudwatch_log_group" "api_users_cwg" {
